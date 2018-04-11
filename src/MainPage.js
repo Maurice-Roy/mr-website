1be8eedafe386a7f2ec6180fb4fd8e4e63be6e8d
Maurice Roy <mautayro@gmail.com>
2018-04-11 16:16:19 -0400

    added soundsystem image with reverse animation

--- src/MainPage.js
+++ src/MainPage.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import TextFace from './TextFace'
+import ReactFitText from 'react-fittext'
+import SoundSystem from './soundsystem.png'
 
 class MainPage extends React.Component {
   render (props) {
@@ -8,9 +10,21 @@ class MainPage extends React.Component {
         <div id="About-link" key="About-page" className="Main-page-block" onClick={(e) => this.props.changeDisplay(e)}>
           <TextFace/>
         </div>
-        <div id="Apps-link" key="Apps-page" className="Main-page-block" onClick={(e) => this.props.changeDisplay(e)}></div>
-        <div id="Audio-link" key="Audio-page" className="Main-page-block" onClick={(e) => this.props.changeDisplay(e)}></div>
-        <div id="Design-link" key="Design-page" className="Main-page-block" onClick={(e) => this.props.changeDisplay(e)}></div>
+        <div id="Apps-link" key="Apps-page" className="Main-page-block" onClick={(e) => this.props.changeDisplay(e)}>
+          <ReactFitText compressor={0}>
+            <span>APPS</span>
+          </ReactFitText>
+        </div>
+        <div id="Audio-link" key="Audio-page" className="Main-page-block" onClick={(e) => this.props.changeDisplay(e)}>
+          <div className="Sound-system-container">
+            <img className="Sound-system" src={SoundSystem} alt="soundsystem"/>
+          </div>
+        </div>
+        <div id="Design-link" key="Design-page" className="Main-page-block" onClick={(e) => this.props.changeDisplay(e)}>
+          <ReactFitText compressor={0}>
+            <span>VISUAL</span>
+          </ReactFitText>
+        </div>
       </div>
     )
   }
